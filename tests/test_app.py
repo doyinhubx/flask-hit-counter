2e0a02ddb46d0e643cfa04ec9cb491cea4b5297b
Doyin <doyinelugbadebo@gmail.com>
2025-07-02 05:48:40 +0100

    fix(ci): ensure all jobs install Flask and test dependencies

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,11 +4,13 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 
 import app
 
+
 def test_home():
     client = app.app.test_client()
     response = client.get("/")
     assert response.status_code == 200
     assert b"You\xe2\x80\x99ve visited this page" in response.data
+
 
 def test_reset():
     client = app.app.test_client()
